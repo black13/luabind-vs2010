22d259bc38eac50dbdacab2624b5baf6fca4cc38
black13 <jjosburn@gmail.com>
2014-01-08 22:51:11 -0600

    changes on this machine

--- lb_call_function/main.cpp
+++ lb_call_function/main.cpp
@@ -11,6 +11,49 @@ extern "C"
 using namespace std;
 using namespace luabind;
 
+#define DOSTRING(state_, str)                   \
+{                                               \
+    try                                         \
+    {                                           \
+        dostring(state_, str);                  \
+    }                                           \
+    catch (luabind::error const& e)             \
+    {                                           \
+        TEST_ERROR(lua_tostring(e.state(), -1)); \
+            lua_pop(L, 1);                      \
+    }                                           \
+    catch (std::string const& s)                \
+    {                                           \
+        TEST_ERROR(s.c_str());                  \
+    }                                           \
+}
+
+#define TEST_REPORT_AUX(x, line, file) \
+	report_failure(x, line, file)
+
+#define TEST_CHECK(x) \
+    if (!(x)) \
+        TEST_REPORT_AUX("TEST_CHECK failed: \"" #x "\"", __FILE__, __LINE__)
+
+#define TEST_ERROR(x) \
+	TEST_REPORT_AUX((std::string("ERROR: \"") + x + "\"").c_str(), __FILE__, __LINE__)
+
+#define TEST_CHECK(x) \
+    if (!(x)) \
+        TEST_REPORT_AUX("TEST_CHECK failed: \"" #x "\"", __FILE__, __LINE__)
+
+#define TEST_ERROR(x) \
+	TEST_REPORT_AUX((std::string("ERROR: \"") + x + "\"").c_str(), __FILE__, __LINE__)
+
+#define TEST_NOTHROW(x) \
+	try \
+	{ \
+		x; \
+	} \
+	catch (...) \
+	{ \
+		TEST_ERROR("Exception thrown: " #x); \
+	}
 int main(int argc, char * argv[])
 {
      lua_State * l = luaL_newstate();
@@ -19,20 +62,27 @@ int main(int argc, char * argv[])
      luabind::open(l);
 
      luaL_dofile(l,".\\visa_sim.lua");
+	 double *p = new double[10];
      try 
      {    
           std::string result;
           //call_function<void>(l, "hello");
           //int ret = call_function<int>(l,"add",1,1);
           //cout << ret << endl;
-          result = call_function<const char*>(l,"concat","1","2");
+          //result = call_function<const char*>(l,"concat","1","2");
+		  /*result = call_function<const char*>(l,"randstring",10);
+
+		  for (int i = 0 ; i < 10; i++ )
+		  {
+			  result[i] 
+		  }*/
      } 
      catch (const error &e) 
      {
           static_cast<void>(e);
           cout << "call_function: " << lua_tostring(l, -1) << endl;
      }
-
+	 delete p;
      lua_close(l);
      return 0;
 }
--- lb_call_function/visa_sim.lua
+++ lb_call_function/visa_sim.lua
@@ -9,3 +9,12 @@ end
 function concat(x,y)
 	return x .. y
 end
+
+function randstring(size)
+	local t = {}
+    for i = 1, size do
+      table.insert(t,math.random())
+    end
+    s = table.concat(t, ",")
+	return s
+end
